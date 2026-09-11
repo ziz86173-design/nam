6b1d10a487df77d079674bb3cc0b79a7afc805ef
ziz86173-design <ziz86173@gmail.com>
2026-09-11 15:27:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
 
+
 API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SESSION_STRING = os.environ["SESSION_STRING"]
 
 BASE_NAME = os.getenv("BASE_NAME", "ZAD")
+
 
 client = TelegramClient(
     StringSession(SESSION_STRING),
@@ -19,24 +21,35 @@ client = TelegramClient(
     API_HASH
 )
 
+
 def to_fancy_digits(text):
     normal_digits = "0123456789"
     fancy_digits = "𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵"
-    digits_map = str.maketrans(normal_digits, fancy_digits)
+
+    digits_map = str.maketrans(
+        normal_digits,
+        fancy_digits
+    )
+
     return str(text).translate(digits_map)
 
+
 async def update_time_name():
+
     await client.connect()
 
     if not await client.is_user_authorized():
-        raise RuntimeError("SESSION_STRING غير صالحة أو انتهت صلاحيتها")
+        raise RuntimeError(
+            "SESSION_STRING غير صالحة أو انتهت صلاحيتها"
+        )
 
     print("تم الاتصال بحساب Telegram")
-    print("السكريبت يعمل الآن (وضع آمن ومستقر)")
+    print("السكريبت يعمل الآن")
 
     last_time = ""
 
     while True:
+
         try:
             raw_time = datetime.datetime.now(
                 ZoneInfo("Africa/Algiers")
@@ -45,6 +58,7 @@ async def update_time_name():
             fancy_time = to_fancy_digits(raw_time)
 
             if fancy_time != last_time:
+
                 new_name = f"{BASE_NAME} {fancy_time}"
 
                 await client(
@@ -54,19 +68,25 @@ async def update_time_name():
                 )
 
                 last_time = fancy_time
+
                 print(f"الاسم أصبح: {new_name}")
 
             await asyncio.sleep(30)
 
         except Exception as e:
+
             print(f"خطأ: {e}")
+
             await asyncio.sleep(60)
+
 
 if __name__ == "__main__":
     try:
         asyncio.run(update_time_name())
+
     except KeyboardInterrupt:
         print("تم إيقاف البرنامج")
+
     except Exception as e:
         print(f"توقف البرنامج: {e}")
         raise
